b0a704b5b3ba82411dae421d9763260260ff3e4b
Punit Juneja <punitjuneja123@gmail.com>
2023-11-29 03:56:02 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const { uploadRouter } = require("./routes/upload.route");
+
+// basic route
+app.get("/", (req, res) => {
+  res.send("Hello :)");
+});
+
+app.use(uploadRouter);
+
+app.listen(process.env.PORT, () => {
+  console.log(`port is rocking at ${process.env.PORT}`);
+});
